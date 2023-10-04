d770a647921536c78b863abcefcb22ca24b9d80c
JANABAK <jgabrielcabana@gmail.com>
2023-10-04 22:35:34 +0800

    Added JsonPropertyNames to City class properties.

--- Weather.Services/ApiSchemas/ApiNinja/City.cs
+++ Weather.Services/ApiSchemas/ApiNinja/City.cs
@@ -9,14 +9,19 @@ namespace Weather.Services.ApiSchemas.ApiNinja
 {
     public class City
     {
+        [JsonPropertyName("name")]
         public string Name { get; set; }
 
+        [JsonPropertyName("latitude")]
         public double Latitude { get; set; }
 
+        [JsonPropertyName("longitude")]
         public double Longitude { get; set; }
 
+        [JsonPropertyName("country")]
         public string Country { get; set; }
 
+        [JsonPropertyName("population")]
         public int Population { get; set; }
 
         [JsonPropertyName("is_capital")]
